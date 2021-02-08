9882d6019fca0136fe7b1244a91dee9189da6ad9
Olivier Gaillard <info@metacoherence.ch>
2021-02-08 10:05:41 +0100

    use env mariadb passwd

--- mariaconnect.sh
+++ mariaconnect.sh
@@ -1,5 +1,3 @@
 #/bin/bash
 
-mysql --host=192.168.1.100 --user=root --port=30306 -p worktime --password=orionScanner103 
-
-#create table hours (leaving DATETIME);
+mysql --host=192.168.1.100 --user=root --port=30306 -p worktime --password=$MARIAPASS
